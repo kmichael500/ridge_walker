2dd6a5679680a2f2d6441b8904ca638a2e71c973
Michael Ketzner <michaelketzner@gmail.com>
2020-02-08 14:04:31 -0600

    working open street maps

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 
+
+
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
