94b33bec25762e8ba5878f2190f27868f7551fcb
Michael Ketzner <michaelketzner@gmail.com>
2020-02-08 22:59:17 -0600

    working point clusters

--- client/src/components/map/MapView.js
+++ client/src/components/map/MapView.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
@@ -9,274 +10,13 @@ import { Map, TileLayer, Marker, Popup, WMSTileLayer, LayersControl, GeoJSON, Ci
 
 import ReactLeafletSearch from "react-leaflet-search";
 import $ from 'jquery';
-import file from '../../assets/file.js'
+import file from '../../assets/tcsv.js'
 
-
-
-var freeBus = {
-  "type": "FeatureCollection",
-  "features": [
-      {
-          "type": "Feature",
-          "geometry": {
-              "type": "LineString",
-              "coordinates": [
-                  [-105.00341892242432, 39.75383843460583],
-                  [-105.0008225440979, 39.751891803969535]
-              ]
-          },
-          "properties": {
-              "popupContent": "This is a free bus line that will take you across downtown.",
-              "underConstruction": false
-          },
-          "id": 1
-      },
-      {
-          "type": "Feature",
-          "geometry": {
-              "type": "LineString",
-              "coordinates": [
-                  [-105.0008225440979, 39.751891803969535],
-                  [-104.99820470809937, 39.74979664004068]
-              ]
-          },
-          "properties": {
-              "popupContent": "This is a free bus line that will take you across downtown.",
-              "underConstruction": true
-          },
-          "id": 2
-      },
-      {
-          "type": "Feature",
-          "geometry": {
-              "type": "LineString",
-              "coordinates": [
-                  [-104.99820470809937, 39.74979664004068],
-                  [-104.98689651489258, 39.741052354709055]
-              ]
-          },
-          "properties": {
-              "popupContent": "This is a free bus line that will take you across downtown.",
-              "underConstruction": false
-          },
-          "id": 3
-      }
-  ]
-};
-
-var lightRailStop = {
-  "type": "FeatureCollection",
-  "features": [
-      {
-          "type": "Feature",
-          "properties": {
-              "popupContent": "18th & California Light Rail Stop"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [-104.98999178409576, 39.74683938093904]
-          }
-      },{
-          "type": "Feature",
-          "properties": {
-              "popupContent": "20th & Welton Light Rail Stop"
-          },
-          "geometry": {
-              "type": "Point",
-              "coordinates": [-104.98689115047453, 39.747924136466565]
-          }
-      }
-  ]
-};
-
-var bicycleRental = {
-  "type": "FeatureCollection",
-  "features": [
-      {
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -104.9998241,
-                  39.7471494
-              ]
-          },
-          "type": "Feature",
-          "properties": {
-              "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-          },
-          "id": 51
-      },
-      {
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -104.9983545,
-                  39.7502833
-              ]
-          },
-          "type": "Feature",
-          "properties": {
-              "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-          },
-          "id": 52
-      },
-      {
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -104.9963919,
-                  39.7444271
-              ]
-          },
-          "type": "Feature",
-          "properties": {
-              "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-          },
-          "id": 54
-      },
-      {
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -104.9960754,
-                  39.7498956
-              ]
-          },
-          "type": "Feature",
-          "properties": {
-              "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-          },
-          "id": 55
-      },
-      {
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -104.9933717,
-                  39.7477264
-              ]
-          },
-          "type": "Feature",
-          "properties": {
-              "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-          },
-          "id": 57
-      },
-      {
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -104.9913392,
-                  39.7432392
-              ]
-          },
-          "type": "Feature",
-          "properties": {
-              "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-          },
-          "id": 58
-      },
-      {
-          "geometry": {
-              "type": "Point",
-              "coordinates": [
-                  -104.9788452,
-                  39.6933755
-              ]
-          },
-          "type": "Feature",
-          "properties": {
-              "popupContent": "This is a B-Cycle Station. Come pick up a bike and pay by the hour. What a deal!"
-          },
-          "id": 74
-      }
-  ]
-};
-
-var campus = {
-  "type": "Feature",
-  "properties": {
-      "popupContent": "This is the Auraria West Campus",
-      "style": {
-          weight: 2,
-          color: "#999",
-          opacity: 1,
-          fillColor: "#B0DE5C",
-          fillOpacity: 0.8
-      }
-  },
-  "geometry": {
-      "type": "MultiPolygon",
-      "coordinates": [
-          [
-              [
-                  [-105.00432014465332, 39.74732195489861],
-                  [-105.00715255737305, 39.74620006835170],
-                  [-105.00921249389647, 39.74468219277038],
-                  [-105.01067161560059, 39.74362625960105],
-                  [-105.01195907592773, 39.74290029616054],
-                  [-105.00989913940431, 39.74078835902781],
-                  [-105.00758171081543, 39.74059036160317],
-                  [-105.00346183776855, 39.74059036160317],
-                  [-105.00097274780272, 39.74059036160317],
-                  [-105.00062942504881, 39.74072235994946],
-                  [-105.00020027160645, 39.74191033368865],
-                  [-105.00071525573731, 39.74276830198601],
-                  [-105.00097274780272, 39.74369225589818],
-                  [-105.00097274780272, 39.74461619742136],
-                  [-105.00123023986816, 39.74534214278395],
-                  [-105.00183105468751, 39.74613407445653],
-                  [-105.00432014465332, 39.74732195489861]
-              ],[
-                  [-105.00361204147337, 39.74354376414072],
-                  [-105.00301122665405, 39.74278480127163],
-                  [-105.00221729278564, 39.74316428375108],
-                  [-105.00283956527711, 39.74390674342741],
-                  [-105.00361204147337, 39.74354376414072]
-              ]
-          ],[
-              [
-                  [-105.00942707061768, 39.73989736613708],
-                  [-105.00942707061768, 39.73910536278566],
-                  [-105.00685214996338, 39.73923736397631],
-                  [-105.00384807586671, 39.73910536278566],
-                  [-105.00174522399902, 39.73903936209552],
-                  [-105.00041484832764, 39.73910536278566],
-                  [-105.00041484832764, 39.73979836621592],
-                  [-105.00535011291504, 39.73986436617916],
-                  [-105.00942707061768, 39.73989736613708]
-              ]
-          ]
-      ]
-  }
-};
-
-var coorsField = {
-  "type": "Feature",
-  "properties": {
-      "popupContent": "Coors Field"
-  },
-  "geometry": {
-      "type": "Point",
-      "coordinates": [-104.99404191970824, 39.756213909328125]
-  }
-};
-
-
-var caveloc = $.ajax({
-  url: process.env.PUBLIC_URL + "cave_loc.json",
-  dataType: "json",
-  success: console.log("County data successfully loaded."),
-  error: function (xhr) {
-    alert(xhr.statusText)
-  }
-})
-
+import MarkerClusterGroup from 'react-leaflet-markercluster/dist/react-leaflet-markercluster';
+import 'react-leaflet-markercluster/dist/styles.min.css';
 
 class MapView extends Component {
 
-
-  
   onLogoutClick = e => {
     e.preventDefault();
     this.props.logoutUser();
@@ -289,16 +29,7 @@ class MapView extends Component {
     };
   }
 
-  onEachFeaturePoint(feature, layer) {
-    console.log('feature: ', feature);
-    console.log('layer: ', layer);
-    layer.on({
-      'click': function (e) {
-         console.log('e: ', e);
-         console.log('click');
-       }
-    })
-  }
+
   
   onEachFeature(feature, layer) {
 		var popupContent = "<p>I started out as a GeoJSON " +
@@ -318,19 +49,17 @@ class MapView extends Component {
     return L.circleMarker(latlng, null); // Change marker to circle
   }
  
-
-
   render() {
     const { user } = this.props.auth;
+    const Markers = 
     
     //39.74739, -105
-    const mapCenterLoc = [35.845600, -86.390300];
-    let blah = this.state;
+    //const mapCenterLoc = [35.845600, -86.390300];
     console.log(file.bicycleRental);
       return (
           <Map
           className="map"
-          center={mapCenterLoc}
+          center={[35.927, -84.264611111]}
           zoom={10}
           maxZoom = {18}
           doubleClickZoom={true}
@@ -340,25 +69,27 @@ class MapView extends Component {
             url="https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw"
           />
           
-        <GeoJSON
-            data={file.caving}
-            color='red'
-            fillColor='green'
-            weight={1}
-            
-            onEachFeature={function(feature, layer) {
-                var popupContent = "<p>" + "<b>" +
-                feature.properties.name + "</b><br>" + "Lat/Long: " + 
-                feature.geometry.coordinates[1] + ", " +
-                feature.geometry.coordinates[0] + "</p>";
+
+        <MarkerClusterGroup>
+            <GeoJSON
+                data={file.caving}
+                color='red'
+                fillColor='green'
+                weight={1}
                 
-                layer.bindPopup(popupContent)
-            }}
-            pointToLayer = {function(feature, latlng){
-                return L.circleMarker(latlng, null)
+                onEachFeature={function(feature, layer) {
+                    var popupContent = "<p>" + "<b>" +
+                    feature.properties.name + "</b><br>" + "Lat/Long: " + 
+                    feature.geometry.coordinates[1] + ", " +
+                    feature.geometry.coordinates[0] + "</p>";
+                    
+                    layer.bindPopup(popupContent)
                 }}
-        />
-          
+                pointToLayer = {function(feature, latlng){
+                    return L.circleMarker(latlng, null)
+                    }}
+            />
+        </MarkerClusterGroup>
         </Map>
       );
   }
