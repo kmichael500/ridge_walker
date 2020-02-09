d5ffbdcb845162aa1054ea455218eb4b4e20822b
Michael Ketzner <michaelketzner@gmail.com>
2020-02-09 16:58:33 -0600

    Updated site text

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -21,8 +21,7 @@ class Dashboard extends Component {
             <h4>
               <b>Hey there,</b> {user.name.split(" ")[0]}
               <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+                Click Map to get started.
               </p>
             </h4>
             <button
