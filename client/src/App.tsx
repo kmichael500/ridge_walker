978054903dd3768dbb19b88271734b7b193a3dcb
Michael Ketzner <michaelketzner@gmail.com>
2020-05-01 18:38:43 -0500

    geojson interface and upload csv

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+/* route imports */
+import { UploadCSV } from './pages/upload'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <div className="App">
+            {/* Protected Routes */}
+            <Switch>
+              {/* Non Protected Routes */}
+              <Route exact path="/upload" component={ UploadCSV } />
+            </Switch>
+          </div>
+        </Router>
     </div>
   );
 }
