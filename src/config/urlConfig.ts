564adf15a2625a8aeef581bcbccd51078163df23
Michael Ketzner <michaelketzner@gmail.com>
2021-01-29 21:29:43 -0600

    added urlConfig to gitignore

--- src/config/urlConfig.ts
+++
@@ -1,6 +0,0 @@
-const serverBaseURL = process.env.REACT_APP_SERVER_BASE_URL
-  ? process.env.REACT_APP_SERVER_BASE_URL
-  : 'http://ridge-walker-server.azurewebsites.net/';
-const siteBaseURL = 'http://192.168.0.105:3000/';
-
-export {serverBaseURL, siteBaseURL};
