cf9be75473fb77eb8dbfe685e6519b02f796426b
Michael Ketzner <michaelketzner@gmail.com>
2021-01-29 20:40:21 -0600

    moved the rest of the files

--- src/config/urlConfig.ts
+++ src/config/urlConfig.ts
@@ -1,6 +1,6 @@
 const serverBaseURL = process.env.REACT_APP_SERVER_BASE_URL
   ? process.env.REACT_APP_SERVER_BASE_URL
-  : 'http://192.168.0.105:5000/';
+  : 'http://ridge-walker-server.azurewebsites.net/';
 const siteBaseURL = 'http://192.168.0.105:3000/';
 
 export {serverBaseURL, siteBaseURL};
